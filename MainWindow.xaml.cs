f0096a4ff8729eacfc22271846bbe3fe7944d7c7
SirCure <36234397+SirCure@users.noreply.github.com>
2018-06-05 22:39:16 -0400

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,13 +23,18 @@ namespace u5GameOfLife
         public MainWindow()
         {
             InitializeComponent();
-
             //Global Variables.
             List<Rectangle> grid = new List<Rectangle>();
+            bool[,] aliveCurrent = new bool[20, 20];
+            bool[,] aliveNext = new bool[20, 20];
+            Point mousePos = new Point();
+            int gridX;
+            int gridY;
 
-            for(int y = 0; y < 20; y++)
+            //Creates the grid out of individual rectangles.
+            for (int y = 0; y < 20; y++)
             {
-                for(int x = 0; x < 20; x++)
+                for (int x = 0; x < 20; x++)
                 {
                     grid.Add(new Rectangle());
                     grid[grid.Count - 1].Height = 40;
@@ -39,8 +44,19 @@ namespace u5GameOfLife
                     canvas.Children.Add(grid[grid.Count - 1]);
                     Canvas.SetLeft(grid[grid.Count - 1], x * 40);
                     Canvas.SetTop(grid[grid.Count - 1], y * 40);
+                    aliveCurrent[x, y] = false;
                 }
             }
+
+            //Allows the user to set the initial values of the cells as true through mouse clicks.
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                MessageBox.Show("Mouse Pressed");
+                mousePos = Mouse.GetPosition(this);
+                //gridX = mousePos.X / 40;
+                //gridY = mousePos.Y / 40;
+                //aliveCurrent[gridX, gridY] = true;
+            } 
         }
     }
 }
